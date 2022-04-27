21de1f755ce9697b22518f1436325a0c2a4106c9
MIRA-KUMAR <mira.kumar.24@gmail.com>
2022-04-27 12:19:27 -0700

    Testing basic calculator

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,21 @@
+from contextlib import AsyncExitStack
 from calculator import Calculator
+
+cal = Calculator()
 
 
 def test_calculator_add():
-    cal = Calculator()
+
     assert cal.add(3, 2) == 5
+
+
+def test_calculator_subtract():
+    assert cal.subtract(9, 3) == 6
+
+
+def test_calculator_multiply():
+    assert cal.multiply(2, 5) == 10
+
+
+def test_calculator_divide():
+    assert float(cal.divide(4, 2)) == 2
